f882aae6de6d832fcc15d6474a3fe7a7e9777717
Kim Dael <daelkdev@gmail.com>
2023-06-08 00:48:12 +0900

    [feature] member domain implementation & test

---
+++ core-study/src/main/java/com/example/corestudy/MemberApp.java
@@ -0,0 +1,18 @@
+package com.example.corestudy;
+
+import com.example.corestudy.member.Grade;
+import com.example.corestudy.member.Member;
+import com.example.corestudy.member.MemberService;
+import com.example.corestudy.member.MemberServiceImpl;
+
+public class MemberApp {
+    public static void main(String[] args) {
+        MemberService memberService = new MemberServiceImpl();
+        Member memberA = new Member(1L, "memberA", Grade.VIP);
+        memberService.join(memberA);
+
+        Member findMember = memberService.findMember(1L);
+        System.out.println("new = " + memberA.getName());
+        System.out.println("findMember = " + findMember.getName());
+    }
+}
